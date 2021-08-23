6d07540937381e75193a36e708c36cfe1b287adb
ky <y.keddaoui@gmail.com>
2021-08-23 02:16:58 +0100

    invoice list menu

--- apps/frontend/src/components/Layout/components/body/LeftMenu.tsx
+++ apps/frontend/src/components/Layout/components/body/LeftMenu.tsx
@@ -9,13 +9,14 @@ import SearchIcon from 'apps/frontend/src/assets/search-icon.png';
 const LeftMenuStyled = styled.div`
   width: 25%;
   height: 100%;
-  padding: 10px 10px;
+  padding: 20px 0;
   background-color: #2f3740;
   text-align: center;
 `;
 
 const Search = styled.div`
-  width: 100%;
+  width: 95%;
+  margin: auto;
   position: relative;
   & input {
     width: 100%;
@@ -37,6 +38,55 @@ const Search = styled.div`
   }
 `;
 
+const InvoiceCount = styled.div`
+  width: 100%;
+  color: #828c98;
+  text-align: left;
+  margin: 20px 0 7px 0;
+  padding: 10px 10px;
+`;
+
+const InvoiceList = styled.div`
+  width: 100%;
+  height: 78%;
+  max-height: 100%;
+  overflow-y: auto;
+`;
+
+const InvoiceBrick = styled.div`
+  width: 100%;
+  height: 100px;
+  padding: 10px 20px;
+  cursor: pointer;
+  & div {
+    display: flex;
+    justify-content: space-between;
+    maring-bottom: 5px;
+  }
+  &:hover {
+    background: #404b57;
+  }
+`;
+
+const P = styled.p`
+  margin: 5px 0;
+  color: #8e98ab;
+  font-weight: lighter !important;
+  font-size: 14px;
+`;
+
+const InvoiceNumber = styled(P)``;
+const InvoiceDate = styled(P)``;
+const NumberOfItems = styled(P)`
+  text-align: left;
+`;
+const CustomerName = styled(P)`
+  color: #5d93db;
+`;
+const GrandTotal = styled(P)`
+  color: white;
+`;
+
 export interface ILeftMenuProps extends StateProps, DispatchProps {}
 
 function LeftMenu(props: ILeftMenuProps) {
@@ -50,12 +100,29 @@ function LeftMenu(props: ILeftMenuProps) {
         <input type="text" placeholder="search..." />
         <img src={SearchIcon} />
       </Search>
+      <InvoiceCount>Invoices: {props.totalNumberOfRecords}</InvoiceCount>
+      <InvoiceList>
+        <InvoiceBrick>
+          <div>
+            <InvoiceNumber>INV.#-1222</InvoiceNumber>
+            <InvoiceDate>{new Date().toDateString()}</InvoiceDate>
+          </div>
+          <div>
+            <NumberOfItems>Items - 05</NumberOfItems>
+          </div>
+          <div>
+            <CustomerName>cutomer name</CustomerName>
+            <GrandTotal>$ 12145</GrandTotal>
+          </div>
+        </InvoiceBrick>
+      </InvoiceList>
     </LeftMenuStyled>
   );
 }
 
 const mapStateToProps = (state: IRootState) => ({
   currentInvoice: state.invoice.currentInvoice,
+  totalNumberOfRecords: state.invoice.totalNumberOfRecords,
 });
 
 const mapDispatchToProps = { getAllInvoices };
